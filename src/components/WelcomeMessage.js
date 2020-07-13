dbe31b2093086d21a5488767dee4f03fd9ebfb3c
Ryan Paul <rpaul06@uoguelph.ca>
2020-07-13 13:33:45 -0400

    added new contact info

--- src/components/WelcomeMessage.js
+++ src/components/WelcomeMessage.js
@@ -24,7 +24,10 @@ function WelcomeMessage(props) {
           <a style={{color:'#1DB954'}} href="https://developer.spotify.com/documentation/web-api/reference/personalization/get-users-top-artists-and-tracks/" target="_blank" rel="noopener noreferrer" role="button"> Spotify's Personalization API Endpoint</a>.
           Each track will also have a unique mashups of gifs based on 
           <a style={{color:'#1DB954'}} href="https://www.tumblr.com/docs/en/api/v2" target="_blank" rel="noopener noreferrer" role="button"> Tumblr's Blog /posts API Endpoint</a>.
-          If you would like to learn more about this website or some of my other projects, check out my 
+          If you would like to learn more about this website please (
+            <a style={{color:'rgb(247, 55, 95)'}} href="mailto:rpaul06@uoguelph.ca"> contact me </a>
+          ) 
+          or some of my other projects, check out my 
           <a style={{color:'rgb(247, 55, 95)'}} href="https://github.com/ryanPaul07" target="_blank" rel="noopener noreferrer" role="button"> Github!</a>
         </Modal.Body>
       </Modal>
