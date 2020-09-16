10f1d7dab136de703241071eb5e7321a4ffa542f
Ryan Paul <rpaul06@uoguelph.ca>
2020-09-16 00:01:44 -0400

    Updates

---
+++ precache-manifest.811fc2248c612eb669329353d4a885a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a7a7f994706cf9dddfcac82cd89424d",
+    "url": "/discovertracks/index.html"
+  },
+  {
+    "revision": "541cb0a38700a983bfad",
+    "url": "/discovertracks/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "abc2f285708aa831a461",
+    "url": "/discovertracks/static/css/main.410b57ed.chunk.css"
+  },
+  {
+    "revision": "541cb0a38700a983bfad",
+    "url": "/discovertracks/static/js/2.abf06944.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/discovertracks/static/js/2.abf06944.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "abc2f285708aa831a461",
+    "url": "/discovertracks/static/js/main.1ff4c938.chunk.js"
+  },
+  {
+    "revision": "80c96853c92538e1dfbc",
+    "url": "/discovertracks/static/js/runtime-main.4a9f22de.js"
+  }
+]);
